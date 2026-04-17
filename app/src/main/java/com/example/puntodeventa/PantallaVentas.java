31afd89015c5bf44c0864e5ae8932e44dabab06d
MiiguelRosales <miiguelangelrv@gmail.com>
2026-04-17 09:31:43 -0600

    Agregar Pantalla de Ventas y botones en Pantalla Inicial

---
+++ app/src/main/java/com/example/puntodeventa/PantallaVentas.java
@@ -0,0 +1,24 @@
+package com.example.puntodeventa;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PantallaVentas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.pantalla_ventas);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainVentas), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
